cd79d74f52d1d5bc1c27a9a25c3e9b0427315111
Kirill Pupkov <pupkovkirill@gmail.com>
2021-06-02 19:33:58 +0500

    Created package with neural network

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 import numpy as np
 import tkinter as tk
-from tkinter import  ttk
 from Blackboard import Blackboard
+from simple_neural_network.NeuralNetwork import NeuralNetwork
 
 IMAGE_SIZE = 28
 
+nn = NeuralNetwork()
+print(nn.get_prediction(0))
+
 window = tk.Tk()
+window.title("Simple Neural Network")
+window.resizable(False, False)
+
 blackboard = Blackboard(window, IMAGE_SIZE)
 
 panel = tk.Frame(window)
@@ -22,5 +28,8 @@ brush_size_list = tk.Spinbox(panel, width=10,
                              textvariable=tk.DoubleVar(value=2))
 brush_size_list.pack(side=tk.LEFT)
 
+prediction_label = tk.Label(panel, text="Hmm... May be it's 5")
+prediction_label.pack(ipadx=100, side=tk.RIGHT, anchor=tk.E)
 
+window.eval('tk::PlaceWindow . center')
 window.mainloop()
---
+++ simple_neural_network/NeuralNetwork.py
@@ -0,0 +1,11 @@
+import numpy as np
+from random import randint as rnd, random
+
+
+class NeuralNetwork:
+
+    def __init__(self):
+        pass
+
+    def get_prediction(self, picture):
+        return rnd(0, 9), random()
